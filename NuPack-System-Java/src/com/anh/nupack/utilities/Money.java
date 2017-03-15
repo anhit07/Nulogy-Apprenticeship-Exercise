89d00aeee2ed47adad42dbc2ff8ec13001b7bf78
Anh Nguyen <Anh Nguyen>
2017-03-14 21:28:07 -0400

    Delete old source code

--- NuPack-System-Java/src/com/anh/nupack/utilities/Money.java
+++
@@ -1,9 +0,0 @@
-package com.anh.nupack.utilities;
-
-public class Money {
-
-	public Money() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- NuPack-System-Spring/src/main/java/com/anh/spring/nupack/utilities/CurrencyUtil.java
+++
@@ -1,9 +0,0 @@
-package com.anh.spring.nupack.utilities;
-
-public final class CurrencyUtil {
-
-	public CurrencyUtil() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
